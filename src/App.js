22cfa0aaaf8dce136237a0ddc9568998a1fb6935
durgesh394 <103554018+durgesh394@users.noreply.github.com>
2022-08-14 17:19:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Wether from "./WetherApp/Wether";
+
+const App = () => {
+ 
+    return(
+        <>
+        <Wether />
+        </>
+    )
+}
+
+export default App;
